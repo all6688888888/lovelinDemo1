c0656e7204f6e4874aa4fd129d658cb9f4556442
root <all6688888888@163.com>
2020-02-19 23:35:17 +0800

    这是我第而次提交代码 demo1666

--- demo1/src/cn/itcast/Aclass.java
+++ demo1/src/cn/itcast/Aclass.java
@@ -5,6 +5,8 @@ public class Aclass {
     public static void main(String[] args) {
         System.out.println("年后呀你好你好");
         System.out.println("知道就好");
+        System.out.println("333");
+        System.out.println("666");
     }
 
 }
